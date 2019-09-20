86c383f1259d508c248cd5a9bce4b886b7b51a9d
arturromero <artur.romero@vix.com>
2019-09-20 18:19:06 -0300

    Elements and UI

---
+++ machinery.js
@@ -0,0 +1,30 @@
+var baldachin;
+var sticky;
+window.onload = function () {
+    window.onscroll = function() {myFunction()};
+
+    baldachin = document.getElementById("baldachin-bar");
+    document.documentElement.scrollTo(0, 0);
+    sticky = baldachin.offsetTop;
+}
+
+
+function myFunction() {
+  if (window.pageYOffset >= sticky) {
+    // console.log('sticky');
+    baldachin.classList.add("sticky");
+  } else {
+    console.log('not sticky');
+    baldachin.classList.remove("sticky");
+  }
+}
+function turnSearch(ele) {
+    ele.classList.toggle("active");
+    if(ele.classList.contains("active")) {
+        document.getElementById('baldachinSearch').focus();
+    }
+}
+function toTop(){
+    console.log('aqui');
+    document.documentElement.scrollTo(0, 0);
+}
