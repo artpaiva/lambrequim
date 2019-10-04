82ad0d6e8945a1ac3ec4b10299e11d68331b179e
arturromero <artur.romero@vix.com>
2019-10-04 19:53:49 -0300

    Additions

--- elements.js
+++ elements.js
@@ -22,7 +22,6 @@ var lucarna = {
     description: 'Lucarna de Chapéu',
   },
 };
-
 var janela = {
   name: 'Janela',
   pivoltante: {
@@ -42,15 +41,42 @@ var janela = {
     description: '',
   },
   serliana: {
-    name: 'Serliana',
+    name: 'Serliana/Paladiana',
     description: '',
   },
   guelfa: {
     name: 'Güelfa',
     description: '',
   },
+  lintel: {
+    name: 'Lintel',
+    description: '',
+  },
+  oriel: {
+    name: 'Oriel',
+    description: '',
+  },
+  monofora: {
+    name: 'Monófora',
+    description: '',
+  },
+  vazista: {
+    name: 'Vazista/Bovenlute',
+    description: '',
+  },
+  antoes: {
+    name: 'Antões',
+    description: '',
+  },
+  jaluzea: {
+    name: 'Jalúsea',
+    description: '',
+  },
+  veisterkassiense: {
+    name: 'Casciêna',
+    description: '',
+  },
 };
-
 var paramento = {
   name: 'Paramento',
   lambril: {
@@ -61,18 +87,226 @@ var paramento = {
     name: 'Silharda',
     description: 'Simulação de uma aparelhação de blocos sobre o paramento',
   },
+  bugnato: {
+    name: 'Bunhato/Rústico',
+    description: 'Aparelhamento de pedras com relevo rústico para construção d\'um paramento',
+  },
   lesena: {
     name: 'Lesena',
     description: 'Relevo vertical sobre o paramento emulando uma pilastra',
   },
+  boaseri: {
+    name: 'Boaserí',
+    description: '',
+  },
+  alegio: {
+    name: 'Alégio',
+    description: '',
+  },
+  chainage: {
+    name: 'Chainagem',
+    description: '',
+  },
+  bardeau: {
+    name: 'Pisarilha/Teroela',
+    description: '',
+  },
+};
+var miscelaneo = {
+  name: 'Miscelâneo',
+  lambril: {
+    name: 'Guilhoche',
+    description: '',
+  },
+  chanfre: {
+    name: 'Chanfre',
+    description: '',
+  },
+  tungewelf: {
+    name: 'Abóbada de Berço/Tonguivél',
+    description: '',
+  },
+  vermiculure: {
+    name: 'Vermiculira',
+    description: '',
+  },
+  torchis: {
+    name: 'Torchíse/Pau-a-Pique',
+    description: '',
+  },
+  stuc: {
+    name: 'Estuque',
+    description: '',
+  },
+  gypserie: {
+    name: 'Gipsería',
+    description: '',
+  },
+};
+var porta = {
+  name: 'Porta',
+  lambril: {
+    name: 'Rastrilho/Hersa',
+    description: 'Porta de grade quadriculada',
+  },
+  chanfre: {
+    name: 'Chambranle',
+    description: '',
+  },
+  volet: {
+    name: 'Volê/Blaffetur',
+    description: '',
+  },
+  vertevelle: {
+    name: 'Verterela',
+    description: 'Fechadura horizontal da Era Gótica',
+  },
+  vantail: {
+    name: 'Vantalhe',
+    description: 'Painel duplo, pivoltante em dobradiças',
+  },
+  ferronerie: {
+    name: 'Ferroneira',
+    description: 'Técnica de elementos arquitetônicos modelada a partir da forja do ferro',
+  },
+};
+var estanca = {
+  name: 'Estança',
+  belver: {
+    name: 'Belver',
+    description: '',
+  },
+  pergola: {
+    name: 'Pérgola',
+    description: '',
+  },
+  orangerie: {
+    name: 'Orangeria/Cidrala',
+    description: '',
+  },
+  aubette: {
+    name: 'Aubete',
+    description: 'Pequena estança exterior destinada a baluarte dos usuários de transporte público rodoviário, esperando seu respectivo veículo (ônibus, táxi)',
+  },
+};
+var exterior = {
+  name: 'Exterior',
+  cachepo: {
+    name: 'Cachepô',
+    description: 'Plantas suspensas',
+  },
+  decalque: {
+    name: 'Decalque/Vetrofânia',
+    description: '',
+  },
+};
+var chao = {
+  name: 'Chão',
+  betonilha: {
+    name: 'Betonilha',
+    description: '',
+  },
+};
+var remate = {
+  name: 'Remate',
+  cimalha: {
+    name: 'Cimalha',
+    description: '',
+  },
+  tecto: {
+    name: 'Tecto',
+    description: '',
+  },
+  cornija: {
+    name: 'Cornija',
+    description: '',
+  },
+  lambrequim: {
+    name: 'Lambrequim',
+    description: '',
+  },
+  dentil: {
+    name: 'Dentil',
+    description: '',
+  },
+  modilhao: {
+    name: 'Modilhão',
+    description: '',
+  },
+  misula: {
+    name: 'Mísula/Corbel',
+    description: '',
+  },
+  sofite: {
+    name: 'Sofite',
+    description: '',
+  },
+  gelosia: {
+    name: 'Gelosia',
+    description: '',
+  },
+  fascia: {
+    name: 'Fáscia',
+    description: '',
+  },
+};
+var luz = {
+  name: 'Iluminação',
+  arandela: {
+    name: 'Arandela',
+    description: '',
+  },
+  candeeiro: {
+    name: 'Candeeiro',
+    description: '',
+  },
+  reverbel: {
+    name: 'Reverbél',
+    description: '',
+  },
+  lustre: {
+    name: 'Lustre',
+    description: '',
+  },
+  lampiao: {
+    name: 'Lampião',
+    description: '',
+  },
+};
+var telhado = {
+  name: 'Telhado',
+  mansarda: {
+    name: 'Mansarda',
+    description: '',
+  },
+  lucarna: {
+    name: 'Lucarna/Abaino',
+    description: '',
+  },
+  beiral: {
+    name: 'Beiral',
+    description: '',
+  },
+  crope: {
+    name: 'Crópe',
+    description: '',
+  },
+  coyel: {
+    name: 'Coyel',
+    description: '',
+  },
+  verriere: {
+    name: 'Claravóia/Lucerna',
+    description: '',
+  },
 };
 
 var recommendedList = document.getElementById('recommendedList');
-var elements = [ lucarna, janela, paramento ];
+var elements = [ lucarna, janela, paramento, miscelaneo, porta, estanca, exterior, chao, remate, luz, telhado ];
 
 function renderElements () {
   var tempElements = elements;
-  for(var i = 0; i < 2; i++) {
+  for(var i = 0; i < 5; i++) {
     var r = Math.floor(Math.random() * tempElements.length);
     var newRecTitle = document.createElement('div');
     newRecTitle.classList.add('frieze-type');
@@ -89,8 +323,6 @@ function tellElement (item) {
   newRecList.classList.add('frieze-list');
   for (var k in item) {
     if(typeof item[k] === 'object'){
-      // console.log(`Name: ${item[k].name}\nDescription: ${item[k].description}`);
-
       var newRecItem = document.createElement('li');
       newRecItem.innerHTML = item[k].name;
       newRecList.appendChild(newRecItem);
